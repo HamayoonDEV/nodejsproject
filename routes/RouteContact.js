5f4f353498f9b3a65085ebd3ebeeca7972c2c753
unknown <hamayoon.47611@iqra.edu.pk>
2023-02-04 21:14:05 +0500

    added new file RouteContact.js and modified server.js

---
+++ routes/RouteContact.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const router = express.Router()
+
+router.route('/').get((req,res)=>{
+    res.status(200).json({message:"get contacts"})
+});
+router.route('/').post((req,res)=>{
+    res.status(200).json({message:"create contacts"})
+});
+router.route('/:id').get((req,res)=>{
+    res.status(200).json({message:`get contacts for ${req.params.id}`})
+});
+
+router.route('/:id').put((req,res)=>{
+    res.status(200).json({message:`update contacts for ${req.params.id}`})
+});
+router.route('/:id').delete((req,res)=>{
+    res.status(200).json({message:`delete contacts for ${req.params.id}`})
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,9 +3,7 @@ const app  = express();
 const dotenv = require("dotenv").config();
 const port  = process.env.PORT || 5000;
 
-app.get('/api/contacts',(req,res)=>{
-    res.status(200).json({message:"this is me hamayoon"})
-})
+app.use('/api/contacts',require("./routes/RouteContact"));
 app.listen(port,()=>{
     console.log(`server is runing on port : ${port}`);
 })
