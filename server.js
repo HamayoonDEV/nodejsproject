ccbfab6deebdea3a836c5a2abb1b7ca688111f96
unknown <hamayoon.47611@iqra.edu.pk>
2023-02-04 20:25:58 +0500

    edited server.js file

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app  = express();
-const port  = 5000;
+const dotenv = require("dotenv").config();
+const port  = process.env.PORT || 5000;
 
-
+app.get('/api/contacts',(req,res)=>{
+    res.status(200).json({message:"this is me hamayoon"})
+})
 app.listen(port,()=>{
-    console.log(`server is runing on port : ${port}`)
+    console.log(`server is runing on port : ${port}`);
 })
